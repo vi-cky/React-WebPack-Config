71e5e1f14e783d265e57740627fefb7eb36a94d6
vijay <vijaybhopalwani@gmail.com>
2021-06-18 13:20:37 +0530

    React Web Pack Configration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+const App = () => {
+  return (
+    <div>
+      <h1>Welcone Vijay, {new Date().toDateString()}</h1>
+    </div>
+  );
+};
+
+export default App;
